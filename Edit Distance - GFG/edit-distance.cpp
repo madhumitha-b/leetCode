391b42b5388805e71b534f1a17a7b284b6e921a9
madhumitha-b <87955195+madhumitha-b@users.noreply.github.com>
2023-09-05 21:43:13 +0530

    Added solution - LeetHub

---
+++ Edit Distance - GFG/edit-distance.cpp
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+     int editDistance(string s, string t) {
+        
+         int n = s.size(), m = t.size(), i = 0, j = 0;
+        int dp[n+1][m+1];
+        for(int i = 0;i<=n;i++)    dp[i][0] = i;
+        for(int i = 0;i<=m;i++)    dp[0][i] = i;
+        for(int i = 1;i<=n;i++)
+        {
+            for(int j = 1;j<=m;j++)
+            {
+              if(s[i-1]== t[j-1])   dp[i][j] = dp[i-1][j-1];
+                
+              else  dp[i][j] = 1+  min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1]));  
+            }
+        }
+       return dp[n][m];
+    }
+
+
+};
+/*
+    gfg
+    gfg
+    
+    int n=s.lenght();
+    int n1=t.lenght();
+    int cnt=0;
+    
+    if(n==n1)
+    {
+        for(int  i=0;i<n;i++){
+           if(s[i]==t[i])
+           continue;
+           else
+           cnt++;
+            
+        }
+    }
+    else{
+        
+        if(n < n1)
+        {
+           for(int  i=0,j=0;i<n,j<n1;){
+           
+           if(s[i]==t[i]){
+               i++;
+               j++;
+           }
+         
+           else{
+           j++;
+           cnt++;
+           }
+             
+        }
+    }
+    else{
+        for(int  i=0,j=0;i<n,j<n1;){
+           
+           if(s[i]==t[i]){
+               i++;
+               j++;
+           }
+         
+           else{
+           i++;
+           cnt++;
+           }
+        
+    }
+    }
+    return cnt;
+    }
+   
+*/
+
+
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string s, t;
+        cin >> s >> t;
+        Solution ob;
+        int ans = ob.editDistance(s, t);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
